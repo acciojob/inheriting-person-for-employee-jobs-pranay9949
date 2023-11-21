0a85f4c10ee180ac2c5d6f435e73729bcf745114
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-11-21 11:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age,greet) {
+  
+}
+Person.prototype.greet=function(){
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+    
+}
+Employee.prototype = Object.create(Person.prototype);
 
+// Fixing the constructor property of the Employee prototype
+Employee.prototype.constructor = Employee;
+
+// Adding jobGreet method to the Employee prototype
+Employee.prototype.jobGreet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
